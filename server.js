805e85bf511c297144dfc98e052a47d3ccf6909a
JFarand <owlasylum@gmail.com>
2017-04-22 16:14:10 -0400

    adding changes for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use((req, res, next) => {
    next();
 });
 //when uncommented this sets site into maintenance mode
-// 
+//
 // app.use((req, res, next) => {
 //    res.render('maintenance.hbs');
 // });
@@ -73,6 +74,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-   console.log('Server is up on port 3000');
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}`);
 });
